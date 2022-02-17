b66d12fc93d3c7dc4a20255ba5c7e148ba925e9e
Bogdan Marinski <bogdanmarinski@gmail.com>
2022-02-17 10:05:12 +0200

    added one solution

--- part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -3,12 +3,28 @@ public class Apartment {
 
     private int rooms;
     private int squares;
-    private int princePerSquare;
+    private int pricePerSquare;
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
-        this.princePerSquare = pricePerSquare;
+        this.pricePerSquare = pricePerSquare;
     }
-
+    
+    private int price() {
+        return this.squares * this.pricePerSquare;
+    }
+    
+    public boolean largerThan(Apartment compared) {
+        return this.squares > compared.squares;
+    }
+    
+    public int priceDifference(Apartment compared) {
+        return Math.abs(this.price() - compared.price());
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared) {
+        return this.price() > compared.price();
+    }
+    
 }
