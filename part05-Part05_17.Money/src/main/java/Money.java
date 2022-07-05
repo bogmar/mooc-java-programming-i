d8643d6f92ae0e61467648359660873480f3994a
Bogdan Marinski <bogdanmarinski@gmail.com>
2022-07-05 22:32:24 +0300

    made some progress at part05-Part05_17.Money but got stuck at the last point

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -31,5 +31,51 @@ public class Money {
 
         return this.euros + "." + zero + this.cents + "e";
     }
+    
+    public Money plus(Money addition) {
+        int totalEuros = this.euros + addition.euros;
+        int totalCents = this.cents + addition.cents;
+        
+        if (totalCents > 100) {
+            totalEuros++;
+            totalCents -= 100;
+        }
+        
+        Money newMoney = new Money(totalEuros, totalCents);
 
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros < compared.euros()) {
+            return true;
+        }
+        if (this.euros == compared.euros()) {
+            if (this.cents < compared.cents()) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Money minus(Money decreaser) {
+        int totalEuros = this.euros - decreaser.euros;
+        int totalCents = this.cents - decreaser.cents;
+        
+        if (totalCents < 0) {
+            totalCents = 100 - totalCents;
+            totalEuros--;
+        }
+        
+        if (totalEuros < 0) {
+            if (totalCents < 0) {
+                totalEuros = 0;
+                totalCents = 0;
+            }
+        }
+        
+        Money newMoney = new Money(totalEuros, totalCents);
+        
+        return newMoney;
+    }
 }
